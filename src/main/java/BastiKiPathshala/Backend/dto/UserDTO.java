faabc49f1a5d3d501b6a5a27798f0f45ff78ae31
Aruj Anand <arujanand@protonmail.com>
2025-08-03 15:14:53 +0530

    Added user DTO

--- src/main/java/BastiKiPathshala/Backend/dto/UserDTO.java
+++ src/main/java/BastiKiPathshala/Backend/dto/UserDTO.java
@@ -1,4 +1,26 @@
 package BastiKiPathshala.Backend.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDTO {
+    @Id
+    private ObjectId id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String username;
+
+    private List<String> roles;
 }
--- src/main/java/BastiKiPathshala/Backend/service/UserService.java
+++ src/main/java/BastiKiPathshala/Backend/service/UserService.java
@@ -18,7 +18,7 @@ public class UserService {
     public boolean saveNewUser(UserEntity newUser) {
         UserEntity userToSave = mapper.map(newUser, UserEntity.class);
         try {
-            userRepository.save(newUser);
+            userRepository.save(userToSave);
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
             throw new RuntimeException("An erro occured while saving the user: " + newUser.getName());
